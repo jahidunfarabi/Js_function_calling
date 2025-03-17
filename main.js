4d918008e6cc14d3503df9ae2e62031b6bd5e667
jahidunfarabi <jahidunmuntaka25@gmail.com>
2025-03-17 20:20:02 +0600

    callback func

--- main.js
+++ main.js
@@ -43,14 +43,14 @@
 //     fetch("https://jsonplaceholder.typicode.com/posts")
 //       .then((res) => res.json())
 //       .then((data) => {
-        
-        
+
 //         console.log(data);
 //       });
 //   });
 // }
 // getdata();
 
+// const promise1 = Pro
 
 
 // const promise1 = Promise.resolve(3);
@@ -64,89 +64,102 @@
 // });
 // Expected output: Array [3, 42, "foo"]
 
-function resolveAfter2Seconds() {
-  console.log("starting slow promise");
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("slow");
-      console.log("slow promise is done");
-    }, 2000);
-  });
-}
+// function resolveAfter2Seconds() {
+//   console.log("starting slow promise");
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve("slow");
+//       console.log("slow promise is done");
+//     }, 2000);
+//   });
+// }
 
-function resolveAfter1Second() {
-  console.log("starting fast promise");
-  return new Promise((resolve) => {
+// function resolveAfter1Second() {
+//   console.log("starting fast promise");
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve("fast");
+//       console.log("fast promise is done");
+//     }, 1000);
+//   });
+// }
+
+// async function sequentialStart() {
+//   console.log("== sequentialStart starts ==");
+
+  // 1. Start a timer, log after it's done
+//   const slow = resolveAfter2Seconds();
+//   console.log(await slow);
+
+//   // 2. Start the next timer after waiting for the previous one
+//   const fast = resolveAfter1Second();
+//   console.log(await fast);
+
+//   console.log("== sequentialStart done ==");
+// }
+
+// async function sequentialWait() {
+//   console.log("== sequentialWait starts ==");
+
+  // // 1. Start two timers without waiting for each other
+  // const slow = resolveAfter2Seconds();
+  // const fast = resolveAfter1Second();
+
+  // // 2. Wait for the slow timer to complete, and then log the result
+  // console.log(await slow);
+  // // 3. Wait for the fast timer to complete, and then log the result
+  // console.log(await fast);
+
+//   console.log("== sequentialWait done ==");
+// }
+
+// async function concurrent1() {
+//   console.log("== concurrent1 starts ==");
+
+//   // 1. Start two timers concurrently and wait for both to complete
+//   const results = await Promise.all([
+//     resolveAfter2Seconds(),
+//     resolveAfter1Second(),
+//   ]);
+//   // 2. Log the results together
+//   console.log(results[0]);
+//   console.log(results[1]);
+
+//   console.log("== concurrent1 done ==");
+// }
+
+// async function concurrent2() {
+//   console.log("== concurrent2 starts ==");
+
+//   // 1. Start two timers concurrently, log immediately after each one is done
+//   await Promise.all([
+//     (async () => console.log(await resolveAfter2Seconds()))(),
+//     (async () => console.log(await resolveAfter1Second()))(),
+//   ]);
+//   console.log("== concurrent2 done ==");
+//}
+
+//sequentialStart(); // after 2 seconds, logs "slow", then after 1 more second, "fast"
+
+// wait above to finish
+//setTimeout(sequentialWait, 4000); // after 2 seconds, logs "slow" and then "fast"
+
+// wait again
+//setTimeout(concurrent1, 7000); // same as sequentialWait
+
+// wait again
+//setTimeout(concurrent2, 10000); // after 1 second, logs "fast", then after 1 more second, "slow"
+
+function f1() {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve("fast");
-      console.log("fast promise is done");
+      console.log();
+      resolve("F1 FUNCTION");
     }, 1000);
   });
 }
+ f1()
+ .then((f1result)=>{
+console.log(f1result);
 
-async function sequentialStart() {
-  console.log("== sequentialStart starts ==");
-
-  // 1. Start a timer, log after it's done
-  const slow = resolveAfter2Seconds();
-  console.log(await slow);
-
-  // 2. Start the next timer after waiting for the previous one
-  const fast = resolveAfter1Second();
-  console.log(await fast);
-
-  console.log("== sequentialStart done ==");
-}
-
-async function sequentialWait() {
-  console.log("== sequentialWait starts ==");
-
-  // 1. Start two timers without waiting for each other
-  const slow = resolveAfter2Seconds();
-  const fast = resolveAfter1Second();
-
-  // 2. Wait for the slow timer to complete, and then log the result
-  console.log(await slow);
-  // 3. Wait for the fast timer to complete, and then log the result
-  console.log(await fast);
-
-  console.log("== sequentialWait done ==");
-}
-
-async function concurrent1() {
-  console.log("== concurrent1 starts ==");
-
-  // 1. Start two timers concurrently and wait for both to complete
-  const results = await Promise.all([
-    resolveAfter2Seconds(),
-    resolveAfter1Second(),
-  ]);
-  // 2. Log the results together
-  console.log(results[0]);
-  console.log(results[1]);
-
-  console.log("== concurrent1 done ==");
-}
-
-async function concurrent2() {
-  console.log("== concurrent2 starts ==");
-
-  // 1. Start two timers concurrently, log immediately after each one is done
-  await Promise.all([
-    (async () => console.log(await resolveAfter2Seconds()))(),
-    (async () => console.log(await resolveAfter1Second()))(),
-  ]);
-  console.log("== concurrent2 done ==");
-}
-
-sequentialStart(); // after 2 seconds, logs "slow", then after 1 more second, "fast"
-
-// wait above to finish
-setTimeout(sequentialWait, 4000); // after 2 seconds, logs "slow" and then "fast"
-
-// wait again
-setTimeout(concurrent1, 7000); // same as sequentialWait
-
-// wait again
-setTimeout(concurrent2, 10000); // after 1 second, logs "fast", then after 1 more second, "slow"
-
+ });
